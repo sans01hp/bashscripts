6494f9c5c620ad10d81010b0521469e6e1901ff6
sans01hp <sans01hp@users.noreply.github.com>
2025-10-03 14:38:43 +0000

    aquatone agora como ferramenta go, instalação automatica de livrarias para scriipts de aprenizado e uso do pipefail para o script recon

--- instaladorlinux.sh
+++ instaladorlinux.sh
@@ -18,19 +18,19 @@ printf "%bEsse script foi feito com o propósito de ser usado no Kali para o Use
 sleep 2
 
 if [ "$(uname)" != "Linux" ]; then
-    printf "%bVocê não está usando um sistema GNU/Linux ou similar%b\n" "$GREEN_BOLD" "$RESET"
-    exit 1
+	printf "%bVocê não está usando um sistema GNU/Linux ou similar%b\n" "$GREEN_BOLD" "$RESET"
+	exit 1
 fi
 
 # Solicita senha sudo uma vez no começo
 printf "%bVerificando permissões de sudo...%b\n" "$CYAN_BOLD" "$RESET"
 sudo -v
 (
-    while true; do
-        sudo -n true
-        sleep 60
-        kill -0 "$$" || exit
-    done
+	while true; do
+		sudo -n true
+		sleep 60
+		kill -0 "$$" || exit
+	done
 ) 2>/dev/null &
 
 # ---------- Atualização do sistema ----------
@@ -46,26 +46,26 @@ printf "%bInstalando linguagens de programação e ferramentas necessárias...%b
 sleep 3
 
 pkg=(
-    python3
-    golang
-    curl
-    unzip
-    wget
-    iputils-ping
-    openssh-client
-    cargo
-    pipx
-    micro
+	python3
+	golang
+	curl
+	unzip
+	wget
+	iputils-ping
+	openssh-client
+	cargo
+	pipx
+	micro
 )
 
 printf "%b[*] Instalando pacotes...%b\n" "$CYAN_BOLD" "$RESET"
 for p in "${pkg[@]}"; do
-    if command -v "${p}" &> /dev/null; then
-        printf "%b[✔] %s já instalado.%b\n" "$GREEN_BOLD" "$p" "$RESET"
-    else
-        printf "%b[ * ] Instalando %s...%b\n" "$YELLOW" "$p" "$RESET"
-        sudo apt install -y "${p}"
-    fi
+	if command -v "${p}" &> /dev/null; then
+		printf "%b[✔] %s já instalado.%b\n" "$GREEN_BOLD" "$p" "$RESET"
+	else
+		printf "%b[ * ] Instalando %s...%b\n" "$YELLOW" "$p" "$RESET"
+		sudo apt install -y "${p}"
+	fi
 done
 
 printf "%bEditor %bmicro %binstalado. Use se precisar de autocomplete para comandos%b\n" "$YELLOW_BOLD" "$GREEN_LIGHT" "$YELLOW" "$RESET"
@@ -80,12 +80,12 @@ MICRO_CONFIG_DIR="${HOME}/.config/micro"
 mkdir -p "${MICRO_CONFIG_DIR}"
 cat > "${MICRO_CONFIG_DIR}/settings.json" <<setup
 {
-    "plugin": ["lsp"],
-    "lspservers": {
-        "python": {
-            "command": "pylsp"
-        }
-    }
+	"plugin": ["lsp"],
+	"lspservers": {
+		"python": {
+			"command": "pylsp"
+		}
+	}
 }
 setup
 
@@ -93,46 +93,47 @@ printf "%bMicro configurado para suporte a Python LSP%b\n" "$GREEN_BOLD" "$RESET
 
 # ---------- Instalando ferramentas Go ----------
 declare -A ferramentas=(
-    ["kxss"]="github.com/Emoe/kxss@latest"
-    ["subfinder"]="github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest"
-    ["httpx"]="github.com/projectdiscovery/httpx/cmd/httpx@latest"
-    ["gau"]="github.com/lc/gau/v2/cmd/gau@latest"
-    ["anew"]="github.com/tomnomnom/anew@latest"
-    ["ffuf"]="github.com/ffuf/ffuf@latest"
-    ["getJS"]="github.com/003random/getJS@latest"
-    ["nuclei"]="github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest"
+	["kxss"]="github.com/Emoe/kxss@latest"
+	["subfinder"]="github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest"
+	["httpx"]="github.com/projectdiscovery/httpx/cmd/httpx@latest"
+	["gau"]="github.com/lc/gau/v2/cmd/gau@latest"
+	["anew"]="github.com/tomnomnom/anew@latest"
+	["ffuf"]="github.com/ffuf/ffuf@latest"
+	["getJS"]="github.com/003random/getJS@latest"
+	["nuclei"]="github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest"
+	["aquatone"]="github.com/michenriksen/aquatone@latest"
 )
 
 printf "%bInstalando ferramentas em Golang...%b\n" "$CYAN" "$RESET"
 sleep 2
 for ferramenta in "${!ferramentas[@]}"; do
-    printf "%bInstalando %b%s%b...%b\n" "$GREEN" "$CYAN_LIGHT" "${ferramenta}" "$GREEN" "$RESET"
-    sleep 1
-    go install -v "${ferramentas[${ferramenta}]}"
+	printf "%bInstalando %b%s%b...%b\n" "$GREEN" "$CYAN_LIGHT" "${ferramenta}" "$GREEN" "$RESET"
+	sleep 1
+	go install -v "${ferramentas[${ferramenta}]}"
 done
 
 # ---------- Clonando repositórios ----------
 declare -A links=(
-    ["ParamSpider"]="https://github.com/devanshbatham/ParamSpider"
-    ["sherlock"]="https://github.com/sherlock-project/sherlock"
-    ["git-dumper"]="https://github.com/arthaud/git-dumper"
-    ["zphisher"]="https://github.com/htr-tech/zphisher"
-    ["sqlmap"]="https://github.com/sqlmapproject/sqlmap"
-    ["https-github.com-Rajkumrdusad-Tool-X"]="https://github.com/vaibhavguru/https-github.com-Rajkumrdusad-Tool-X.git"
-    ["codigos_para_aprendizado"]="https://github.com/sans01hp/codigos_para_aprendizado"
-    ["nuclei-templates"]="https://github.com/projectdiscovery/nuclei-templates"
+	["ParamSpider"]="https://github.com/devanshbatham/ParamSpider"
+	["sherlock"]="https://github.com/sherlock-project/sherlock"
+	["git-dumper"]="https://github.com/arthaud/git-dumper"
+	["zphisher"]="https://github.com/htr-tech/zphisher"
+	["sqlmap"]="https://github.com/sqlmapproject/sqlmap"
+	["https-github.com-Rajkumrdusad-Tool-X"]="https://github.com/vaibhavguru/https-github.com-Rajkumrdusad-Tool-X.git"
+	["codigos_para_aprendizado"]="https://github.com/sans01hp/codigos_para_aprendizado"
+	["nuclei-templates"]="https://github.com/projectdiscovery/nuclei-templates"
 )
 
 printf "%bBaixando repositórios necessários...%b\n" "$CYAN" "$RESET"
 for repo in "${!links[@]}"; do
-    if [ ! -d "${repo}" ]; then
-        printf "%bClonando %s...%b\n" "$CYAN_LIGHT" "${repo}" "$RESET"
-        git clone "${links[${repo}]}"
-    else
-        printf "%bAtualizando repositório %b%s%b...%b\n" "$GREEN" "$CYAN_LIGHT" "${repo}" "$GREEN" "$RESET"
-        git -C "${repo}" reset --hard
-        git -C "${repo}" pull
-    fi
+	if [ ! -d "${repo}" ]; then
+		printf "%bClonando %s...%b\n" "$CYAN_LIGHT" "${repo}" "$RESET"
+		git clone "${links[${repo}]}"
+	else
+		printf "%bAtualizando repositório %b%s%b...%b\n" "$GREEN" "$CYAN_LIGHT" "${repo}" "$GREEN" "$RESET"
+		git -C "${repo}" reset --hard
+		git -C "${repo}" pull
+	fi
 done
 
 # ---------- Ambiente virtual Python ----------
@@ -140,38 +141,40 @@ python3 -m venv "${HOME}/piplibs"
 source "${HOME}/piplibs/bin/activate"
 
 for repo in "${!links[@]}"; do
-    REPO_PATH="./${repo}"
-    if [ -f "${REPO_PATH}/setup.py" ] || [ -f "${REPO_PATH}/pyproject.toml" ]; then
-        printf "%bTentando instalar %s com pip%b\n" "$GREEN_BOLD" "${repo}" "$RESET"
-        pip install "${REPO_PATH}" || printf "%bFalha ao instalar %s com pip. Instale manualmente se necessário.%b\n" "$RED_BOLD" "${repo}" "$RESET"
-        if [ -f "${HOME}/piplibs/bin/${repo}" ]; then
-            sudo ln -sf "${HOME}/piplibs/bin/${repo}" /usr/local/bin/"${repo}"
-        fi
-    else
-        printf "%bRepositório %s não é um pacote Python instalável. Instalação manual será necessária.%b\n" "$YELLOW_BOLD" "${repo}" "$RESET"
-    fi
+	REPO_PATH="./${repo}"
+	if [ -f "${REPO_PATH}/setup.py" ] || [ -f "${REPO_PATH}/pyproject.toml" ]; then
+		printf "%bTentando instalar %s com pip%b\n" "$GREEN_BOLD" "${repo}" "$RESET"
+		pip install "${REPO_PATH}" || printf "%bFalha ao instalar %s com pip. Instale manualmente se necessário.%b\n" "$RED_BOLD" "${repo}" "$RESET"
+		if [ -f "${HOME}/piplibs/bin/${repo}" ]; then
+			sudo ln -sf "${HOME}/piplibs/bin/${repo}" /usr/local/bin/"${repo}"
+		fi
+	else
+		printf "%bRepositório %s não é um pacote Python instalável. Instalação manual será necessária.%b\n" "$YELLOW_BOLD" "${repo}" "$RESET"
+	fi
 done
 
 deactivate
 
-# ---------- Bloco de instalação do Aquatone ----------
-if [ -d "${HOME}/aquatone" ] && [ -f "${HOME}/aquatone/aquatone" ]; then
-    printf "%bAquatone já está instalado em %s%b\n" "$GREEN_BOLD" "${HOME}/aquatone" "$RESET"
+# ---------- Ambiente virtual de codigos_para_aprendizado  ----------
+path4env="${HOME}/codigos_para_aprendizado/python3"
+if [[ -d "${path4env}" ]]; then
+	printf "%bCriando ambiente virtual em %b%s%b\n" "${YELLOW_BOLD}" "${GREEN_BOLD}" "${path4env}" "${RESET}"
+	python3 -m venv "${path4env}/libs"
+	source "${path4env}/libs/bin/activate"
+	pip install -r "${path4env}/requirements.txt" #2> /dev/null
+	if [[ $? -eq 0 ]]; then
+		printf "%bSucesso ao instalar livrarias%b\n" "${GREEN_BOLD}" "${RESET}"
+	else
+		printf "%bFalha na instalação de livrarias%b\n" "${RED_BOLD}" "${RESET}"
+	fi
 else
-    {
-        mkdir -p "${HOME}/aquatone"
-        cd "${HOME}/aquatone" || exit
-        wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
-        unzip aquatone_linux_amd64_1.7.0.zip
-        chmod +x aquatone
-        printf "%bAquatone instalado com sucesso%b\n" "$GREEN_BOLD" "$RESET"
-    }
+	printf "%b[AVISO]%bO PATH ${path4env} não foi encontrado.\n" "${YELLOW_BOLD}" "${RESET}"
 fi
 
 # ---------- Links simbólicos para Go ----------
 for go_tool in "${HOME}/go/bin/"*; do
-    tool_name=$(basename "${go_tool}")
-    sudo ln -sf "${go_tool}" /usr/local/bin/"${tool_name}"
+	tool_name=$(basename "${go_tool}")
+	sudo ln -sf "${go_tool}" /usr/local/bin/"${tool_name}"
 done
 
 printf "%bAviso: %bAs ferramentas em Golang foram linkadas para /usr/local/bin para facilitar o uso das mesmas.%b\n" "$YELLOW" "$GREEN" "$RESET"
--- recon.sh
+++ recon.sh
@@ -15,92 +15,97 @@ RESET="\033[0m"
 
 # ---------- Funções ----------
 menu() {
-    printf "%b1-Recon completo (Subfinder + Httpx + Gau)%b\n" "$GREEN" "$RESET"
-    printf "%b2-Usar Nuclei %b[ROOT NECESSÁRIO]%b\n" "$PURPLE" "$RED" "$RESET"
-    printf "%b4-Achar informações no JavaScript%b\n" "$CYAN_LIGHT" "$RESET"
-    printf "%b9-Mudar alvo%b\n" "$RED" "$RESET"
-    printf "%b00-Sair%b\n" "$YELLOW" "$RESET"
+	printf "%b1-Recon completo (Subfinder + Httpx + Gau)%b\n" "$GREEN" "$RESET"
+	printf "%b2-Usar Nuclei %b[ROOT NECESSÁRIO]%b\n" "$PURPLE" "$RED" "$RESET"
+	printf "%b4-Achar informações no JavaScript%b\n" "$CYAN_LIGHT" "$RESET"
+	printf "%b9-Mudar alvo%b\n" "$RED" "$RESET"
+	printf "%b00-Sair%b\n" "$YELLOW" "$RESET"
 }
 
 recon_all() {
-    printf "%b[INFO]%b Rodando Subfinder + Httpx + Gau em: %s\n" "$GREEN" "$RESET" "${url}"
+	printf "%b[INFO]%b Rodando Subfinder + Httpx + Gau em: %s\n" "$GREEN" "$RESET" "${url}"
 
-    output_dir="${HOME}/bashscripts/subd_results"
-    mkdir -p "$output_dir"
+	output_dir="${HOME}/bashscripts/subd_results"
+	mkdir -p "$output_dir"
 
-    domain="${url#*://}"    # remove http:// ou https://
-    domain="${domain%%/*}"  # remove caminho/paths
+	domain="${url#*://}"    # remove http:// ou https://
+	domain="${domain%%/*}"  # remove caminho/paths
 
-    out_file="${output_dir}/${domain}.txt"
+	out_file="${output_dir}/${domain}.txt"
 
-    printf "%b[INFO]%b Salvando resultados em: %s\n" "$CYAN_LIGHT" "$RESET" "$out_file"
+	printf "%b[INFO]%b Salvando resultados em: %s\n" "$CYAN_LIGHT" "$RESET" "$out_file"
+	# pipefail para capturar erros na execução
+	set -o pipefail
 
-    # pipeline simples, resultado mostrado e salvo com tee
-    subfinder -d "$domain" -silent \
-      | httpx -silent \
-      | gau \
-      | tee "$out_file"
+	# pipeline simples, resultado mostrado e salvo com tee
+	subfinder -d "$domain" -silent \
+		| httpx -silent \
+		| gau \
+		| tee "$out_file"
 
-    status=$?
+	status=$?
+	# desativando o pipefail para não continuar no resto do script
+	set +o pipefail
 
-    if [[ $status -eq 0 ]]; then
-        printf "%b[OK]%b Recon concluído. Arquivo: %s\n" "$GREEN" "$RESET" "$out_file"
-        return 0
-    else
-        printf "%b[ERRO]%b Falha durante o recon para %s (status %d)\n" "$RED" "$RESET" "${url}" "$status"
-        return $status
-    fi
+	if [[ $status -eq 0 ]]; then
+		printf "%b[OK]%b Recon concluído. Arquivo: %s\n" "$GREEN" "$RESET" "$out_file"
+		return 0
+	else
+		printf "%b[ERRO]%b Falha durante o recon para %s (status %d)\n" "$RED" "$RESET" "${url}" "$status"
+		return $status
+	fi
 }
+
 javascript() {
-    printf "%b[INFO]%b Coletando informações no JavaScript...\n" "$GREEN" "$RESET"
-    printf "%s\n" "${url}" | getJS
+	printf "%b[INFO]%b Coletando informações no JavaScript...\n" "$GREEN" "$RESET"
+	printf "%s\n" "${url}" | getJS
 }
 
 nuclei() {
-    printf "%bQuais templates quer usar?%b\n" "$YELLOW" "$RESET"
-    printf "1-todos\n"
-    printf "2-exposures\n"
-    printf "3-cves\n"
-    printf "4-exposed panels\n"
-    printf "5-fuzzing\n"
-    printf "6-vulnerabilities\n"
+	printf "%bQuais templates quer usar?%b\n" "$YELLOW" "$RESET"
+	printf "1-todos\n"
+	printf "2-exposures\n"
+	printf "3-cves\n"
+	printf "4-exposed panels\n"
+	printf "5-fuzzing\n"
+	printf "6-vulnerabilities\n"
 
-    read -r template
+	read -r template
 
-    case "$template" in
-        1) "$HOME/go/bin/nuclei" -u "${url}" -t "${HOME}/nuclei-templates" ;;
-        2) "$HOME/go/bin/nuclei" -u "${url}" -t "${HOME}/nuclei-templates/exposures" ;;
-        3) "$HOME/go/bin/nuclei" -u "${url}" -t "${HOME}/nuclei-templates/cves" ;;
-        4) "$HOME/go/bin/nuclei" -u "${url}" -t "${HOME}/nuclei-templates/exposed-panels" ;;
-        5) "$HOME/go/bin/nuclei" -u "${url}" -t "${HOME}/nuclei-templates/fuzzing" ;;
-        6) "$HOME/go/bin/nuclei" -u "${url}" -t "${HOME}/nuclei-templates/vulnerabilities" ;;
-        *)
-            printf "%bOpção inválida %b%s%b\n" "$YELLOW" "$CYAN_LIGHT" "(╯°□°）╯︵┻━┻" "$RESET"
-            return ;;
-    esac
+	case "$template" in
+		1) "$HOME/go/bin/nuclei" -u "${url}" -t "${HOME}/nuclei-templates" ;;
+		2) "$HOME/go/bin/nuclei" -u "${url}" -t "${HOME}/nuclei-templates/exposures" ;;
+		3) "$HOME/go/bin/nuclei" -u "${url}" -t "${HOME}/nuclei-templates/cves" ;;
+		4) "$HOME/go/bin/nuclei" -u "${url}" -t "${HOME}/nuclei-templates/exposed-panels" ;;
+		5) "$HOME/go/bin/nuclei" -u "${url}" -t "${HOME}/nuclei-templates/fuzzing" ;;
+		6) "$HOME/go/bin/nuclei" -u "${url}" -t "${HOME}/nuclei-templates/vulnerabilities" ;;
+		*)
+			printf "%bOpção inválida %b%s%b\n" "$YELLOW" "$CYAN_LIGHT" "(╯°□°）╯︵┻━┻" "$RESET"
+			return ;;
+	esac
 }
 
 resetar_url() {
-    printf "%bDigite o %bDominio/Url %bque deseja analisar:%b\n" "$YELLOW" "$CYAN_LIGHT" "$YELLOW" "$RESET"
-    read -r url
+	printf "%bDigite o %bDominio/Url %bque deseja analisar:%b\n" "$YELLOW" "$CYAN_LIGHT" "$YELLOW" "$RESET"
+	read -r url
 
-    if [[ -z "${url}" ]]; then
-        printf "%b[ERRO]%b URL vazia.%b\n" "$RED" "$YELLOW" "$RESET"
-        return 1
-    fi
+	if [[ -z "${url}" ]]; then
+		printf "%b[ERRO]%b URL vazia.%b\n" "$RED" "$YELLOW" "$RESET"
+		return 1
+	fi
 
-    if [[ "${url}" != https://* && "${url}" != http://* ]]; then
-        url="https://${url}"
-    fi
+	if [[ "${url}" != https://* && "${url}" != http://* ]]; then
+		url="https://${url}"
+	fi
 
-    # regex simplificado e portátil
-    if [[ "${url}" =~ ^https?://([A-Za-z0-9.-]+\.[A-Za-z]{2,})(/.*)?$ ]]; then
-        printf "%bAtualmente analisando o link: %b%s%b\n" "$YELLOW" "$CYAN_LIGHT" "${url}" "$RESET"
-        return 0
-    else
-        printf "%b[ERRO]%b Dominio ou subdominio %s invalido.%b\n" "$RED" "$YELLOW" "${url}" "$RESET"
-        return 2
-    fi
+	# regex simplificado e portátil
+	if [[ "${url}" =~ ^https?://([A-Za-z0-9.-]+\.[A-Za-z]{2,})(/.*)?$ ]]; then
+		printf "%bAtualmente analisando o link: %b%s%b\n" "$YELLOW" "$CYAN_LIGHT" "${url}" "$RESET"
+		return 0
+	else
+		printf "%b[ERRO]%b Dominio ou subdominio %s invalido.%b\n" "$RED" "$YELLOW" "${url}" "$RESET"
+		return 2
+	fi
 }
 
 # ---------- Entrada inicial ----------
@@ -108,55 +113,56 @@ output=""
 url=""
 
 while getopts "u:o:h" flag; do
-    case "$flag" in
-       h)
-          echo "Forma de uso: $0 -u <url> -o <nome_do_arquivo_de_saida.txt> (opcional)"
-          echo "-u      define a url inicial (ex: -u exemplo.com ou -u https://exemplo.com)"
-          echo "-h      mostra esse texto"
-          echo "-o      output para fuzzing (ex: -o resultado.txt)"
-          exit 0
-          ;;
-       o) output=$OPTARG ;;
-       u)
-          url=$OPTARG
-          # normalize
-          if [[ "${url}" != https://* && "${url}" != http://* ]]; then
-              url="https://${url}"
-          fi
-          if ! [[ "${url}" =~ ^https?://([A-Za-z0-9.-]+\.[A-Za-z]{2,})(/.*)?$ ]]; then
-              printf "%b[ERRO]%b Dominio ou subdominio %s invalido.%b\n" "$RED" "$YELLOW" "${url}" "$RESET"
-              exit 2
-          fi
-          ;;
-       ?)
-          echo "Opção inválida. Use -h para ajuda."
-          exit 1
-          ;;
-    esac
+	case "$flag" in
+		h)
+			echo "Forma de uso: $0 -u <url> -o <nome_do_arquivo_de_saida.txt> (opcional)"
+			echo "-u      define a url inicial (ex: -u exemplo.com ou -u https://exemplo.com)"
+			echo "-h      mostra esse texto"
+			echo "-o      output para fuzzing (ex: -o resultado.txt)"
+			exit 0
+			;;
+		o) output=$OPTARG ;;
+		u)
+			url=$OPTARG
+			# normalize
+			if [[ "${url}" != https://* && "${url}" != http://* ]]; then
+				url="https://${url}"
+			fi
+			if ! [[ "${url}" =~ ^https?://([A-Za-z0-9.-]+\.[A-Za-z]{2,})(/.*)?$ ]]; then
+				printf "%b[ERRO]%b Dominio ou subdominio %s invalido.%b\n" "$RED" "$YELLOW" "${url}" "$RESET"
+				exit 2
+			fi
+			;;
+		?)
+			echo "Opção inválida. Use -h para ajuda."
+			exit 1
+			;;
+	esac
 done
 
 # verifica se a variavel está vazia
 if [[ -z "${url}" ]]; then
-    printf "%bA flag -u não pode ser vazia%b\n" "${YELLOW}" "${RESET}"
+	printf "%bA flag -u não pode ser vazia%b\n" "${YELLOW}" "${RESET}"
 	printf "%bUse $0 -u <url ou dominio>%b" "${GREEN}" "${RESET}"
-	exit 1 
-fi	
+	exit 1
+fi
+
 # ---------- Loop principal ----------
 while true; do
-    menu
-    printf "%bDigite o numero da opção que você quer%b:" "${GREEN}" "${RESET}"
-    read -r  opcao
-    case "$opcao" in
-        1) recon_all ;;
-        2) nuclei ;;
-        4) javascript ;;
-        9) resetar_url ;;
-        00)
-            printf "%bSaindo...%b\n" "$YELLOW" "$RESET"
-            exit 0
-            ;;
-        *)
-            printf "%bOpção inválida %b%s%b\n" "$YELLOW" "$CYAN_LIGHT" "(╯°□°）╯︵┻━┻" "$RESET"
-            ;;
-    esac
+	menu
+	printf "%bDigite o numero da opção que você quer%b:" "${GREEN}" "${RESET}"
+	read -r opcao
+	case "$opcao" in
+		1) recon_all ;;
+		2) nuclei ;;
+		4) javascript ;;
+		9) resetar_url ;;
+		00)
+			printf "%bSaindo...%b\n" "$YELLOW" "$RESET"
+			exit 0
+			;;
+		*)
+			printf "%bOpção inválida %b%s%b\n" "$YELLOW" "$CYAN_LIGHT" "(╯°□°）╯︵┻━┻" "$RESET"
+			;;
+	esac
 done
